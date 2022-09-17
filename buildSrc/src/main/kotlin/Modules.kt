ea41c60470ab01736e2ddd3c07c6b35f7f6dce85
sammy-mutahi <sammymutahi21@gmail.com>
2022-09-17 17:00:33 +0300

    modify build source objects

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,4 +1,4 @@
 object Modules {
-    const val presentation = ":forecast:forecast_presentation"
-    const val domain = ":forecast:forecast_domain"
+    const val presentation = ":milkyway:milkyway_presentation"
+    const val domain = ":milkyway:milkyway_domain"
 }
--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,5 +1,5 @@
 object ProjectConfig {
-    const val appId = "com.payoneer.weatherforecast"
+    const val appId = "com.sammy.milkywayimages"
     const val compileSdk = 32
     const val minSdk = 21
     const val targetSdk = 32
